3de9c3c4a37642812bc57da2eb75957733c48cdb
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-07-14 20:19:31 +0100

    playing with api

---
+++ Data Analysis/RestCountriesAPI.py
@@ -0,0 +1,45 @@
+import requests,json,pprint
+pretty = pprint.PrettyPrinter(indent = 2)
+
+#API used for this is:
+#https://restcountries.eu/#filter-response
+def tag_to_name(country_dict) -> list:
+    #fetches full country names from api
+    country_tags = country_dict["bordering_countries"]
+    list1=[]
+    
+    for tag in country_tags:
+        request = requests.get('https://restcountries.eu/rest/v2/alpha/'+tag)
+        json_text = json.dumps(request.json())
+        request_response = json.loads(json_text)
+        list1.append(request_response['name'])
+        
+    return list1
+
+def name(country:str) -> dict:
+    #request = requests.get('https://restcountries.eu/rest/v2/region/europe')
+    #country='pol'
+    request = requests.get('https://restcountries.eu/rest/v2/alpha/'+country)
+    #stores data from api in data
+    #converst data to element
+    json_text = json.dumps(request.json())
+    #converst it to string format
+    request_response = json.loads(json_text)
+
+    country_tags = {"bordering_countries":request_response['borders']}
+    
+    country_dict = { "country_name":request_response['name'],
+                  #languages used retrives a list of dictionaries hence the [0] to get the first dictionary object and access the name key
+                  "bordering_countries":tag_to_name(country_tags),
+                  "national_language":request_response['languages'][0]['name']}
+    
+    return country_dict
+
+
+print(name("pol"))
+    
+
+
+    
+
+
