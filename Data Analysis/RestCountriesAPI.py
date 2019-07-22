fceb452513e91a2c4b79ca7a8eb399c717d1eef1
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-07-22 19:50:00 +0100

    comment

--- Data Analysis/RestCountriesAPI.py
+++ Data Analysis/RestCountriesAPI.py
@@ -6,14 +6,17 @@ import logging
 import time
 
 
-class Look_up:
+class Look_up():
     # TODO use db , use logger more 
-    
+    def __init__(self,country: str = "",country_dict: dict = dict()) -> None:
+        self.country = country
+        self.country_dict = country_dict
+        
     # class Country_lookup(country):
-    def look_up_api(country: str) -> json:
+    def look_up_api(country):
         """shorthand look up for api"""
         # sends response code for request
-        response = requests.get("https://restcountries.eu/rest/v2/alpha/"+country)
+        response = requests.get("https://restcountries.eu/rest/v2/alpha/"+ country)
         
         if response.status_code == 200:
             # stores data from api in data
@@ -61,9 +64,10 @@ class Look_up:
         # logging.basicConfig(filename = "path", level = logging.DEBUG, format = LOG_FORMAT, filemode = 'w')
         # logger = logging.getLogger()
         
-        request_response = look_up_api(country)
+        request_response = Look_up.look_up_api(country)
         
         try:
+            # fills dict with abreveations of coutnry names "DE" for germany
             country_tags = {"bordering_countries": request_response['borders']}
         except Exception as err:
             print("No bordering countries")
@@ -71,23 +75,23 @@ class Look_up:
             raise
         try:
             country_dict = {"country_name": request_response['name'],
+                            # converts tag "DE" to germany country name
+                            "bordering_countries": Look_up.tag_to_name(country_tags),
                             # languages is a list of dictionaries
-                            "bordering_countries": tag_to_name(country_tags),
                             "main_language": request_response['languages'][0]['name']}
-            return pprint.pprint(country_dict)
+            return country_dict
         except Exception as err:
             print("Country with name of " + "'" + country + "'" + " not found , please try another variation")
             print(err)
             raise
         finally:
+            # tracks runtime of request
             stop_time = time.time()
             time_stamp = stop_time - start_time
             # limit to x decimal places
-            print("run time %.5f" % time_stamp + " seconds")
-            
+            print("run time %.5f" % time_stamp + " seconds")        
     
         
-    
     def eu() -> print:
     
         request = requests.get('https://restcountries.eu/rest/v2/region/europe')
@@ -101,5 +105,4 @@ class Look_up:
             except:
                 print("error retrieving data from region EU")
 
-country_lookup("de")
-
+# pprint.pprint(Look_up.country_lookup())
