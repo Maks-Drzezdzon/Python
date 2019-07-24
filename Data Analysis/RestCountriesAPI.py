39d60cf800e44c7f846f9ff8d09bbf32d2735c5c
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-07-24 21:55:39 +0100

    looking up some python tools , cleaning up code in the mean time

--- Data Analysis/RestCountriesAPI.py
+++ Data Analysis/RestCountriesAPI.py
@@ -5,16 +5,21 @@ import pyodbc as db
 import logging
 import time
 
+        
+'''conn = db.connect("Driver={SQL Server Native Client 11.0};"
+                      "Server=;"
+                      "Database=;"
+                      "Trusted_Connection=yes;")''' 
+
 
 class Look_up():
     # TODO use db , use logger more 
-    # def __init__(self,country: str = "",country_dict: dict = dict()) -> None:
-        # self.country = country
-        # self.country_dict = country_dict
+    def __init__(self,country_dict) -> None:
+         self.country_dict = country_dict
         
-    # class Country_lookup(country):
-    def look_up_api(country):
-        """shorthand look up for api"""
+        # class Country_lookup(country):
+    def look_up_api(country: str):
+        """Shorthand look up for api, expected input looks like DE,POL,IE"""
         # sends response code for request
         response = requests.get("https://restcountries.eu/rest/v2/alpha/"+ country)
         
@@ -22,25 +27,21 @@ class Look_up():
             # stores data from api in data
             # converst data to element
             # dumps produces text output
-            json_text = json.dumps(response.json(), indent = 4)
+            # json_text = json.dumps(response.json(), indent = 4)
             # converst it to string format
             # loads creates object from that text representation
-            request_response = json.loads(json_text)
+            # request_response = json.loads(response.text)
+            return response.json()
         else:
             # adds an extra second to execution with code check 
             print("error " + response.status_code)
-        return request_response
-        
-    '''conn = db.connect("Driver={SQL Server Native Client 11.0};"
-                      "Server=;"
-                      "Database=;"
-                      "Trusted_Connection=yes;")'''    
+           
         # API used for this is:
         # https://restcountries.eu/#filter-response
         
     def tag_to_name(country_dict) -> list:
         """returns full country name from json dict, DE = germany"""
-        # fetches full country names from api
+        # fetches full country names from api    
         country_tags = country_dict["bordering_countries"]
         list_of_countries = list()
         for tag in country_tags:
@@ -55,7 +56,8 @@ class Look_up():
         return list_of_countries
         
         
-    def country_lookup(country: str) -> dict:
+    def country(country: str) -> dict:
+        """query the restcountries.eu api using this method by passing in a str with a value similar to POL, DE, IE"""
         # add functionallity so that a name like germany gets translated to a tag,
         # possibily with a try excpet finally to check if germany resolves to DE
         
@@ -93,16 +95,19 @@ class Look_up():
     
         
     def eu() -> print:
-    
+        """work in prog, queries the api for a whole region"""
         request = requests.get('https://restcountries.eu/rest/v2/region/europe')
-        json_text = json.dumps(request.json())
-        request_response = json.loads(json_text)
-        
-        for element in request_response:
+        # json_text = json.dumps(request.json())
+        # request_response = json.loads(request.text)
+        response = request.json()
+        for element in request.json():
             try:
-                pprint.pprint(request_response[0])
+                pprint.pprint(response[0])
                 # pprint.pprint(request_response[0][element]['name'])
             except:
                 print("error retrieving data from region EU")
 
-# pprint.pprint(Look_up.country_lookup())
+pprint.pprint(Look_up.country_lookup("pol"))
+#Look_up.eu()
+
+
