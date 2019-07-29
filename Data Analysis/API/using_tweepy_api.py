c0522e89e639863aea0ef05278ebb4956c18e010
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-07-29 20:54:51 +0100

    done for now

--- Data Analysis/API/using_tweepy_api.py
+++ Data Analysis/API/using_tweepy_api.py
@@ -1,9 +1,16 @@
 import tweepy 
 import os
 import sys
+import time
+from bs4 import BeautifulSoup
 
-keys_file = open("C:/Users/Grim/Desktop/twitter_key.txt")
-text = keys_file.readlines()
+soup = BeautifulSoup(PLACE_HOLDER.text,'html.parser')
+# finds pattern in html with tags span class short-desc
+results = soup.find_all('span', attrs={'class' : 'short-desc'})
+
+
+with open("C:/Users/Grim/Desktop/twitter_key.txt") as keys_file:
+    text = keys_file.readlines()
 
 consumer_key = text[0].strip().split('"')[1]
 consumer_secret = text[1].strip().split('"')[1]
@@ -14,6 +21,9 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+time_stamp = time.ctime()
+api.update_status("api test on " + time_stamp)
+
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
-    print (tweet.text)
+    print (f"{tweet.user.name} said {tweet.text}")
