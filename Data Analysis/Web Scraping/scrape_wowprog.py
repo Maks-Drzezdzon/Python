c1454008ca11df76487575d4fd01b4238278184e
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-07-30 18:12:13 +0100

    putting together data

--- Data Analysis/Web Scraping/scrape_wowprog.py
+++ Data Analysis/Web Scraping/scrape_wowprog.py
@@ -9,15 +9,39 @@ store=[]
     
 mythic_proggress = soup.find('div',{'class':'sortBy'}) # opens div with raid names
 table = soup.find('table',{'class':'rating'}) # opens table with team data
-data = table.find('a',{'class':'guild'}) # opens row with team data
-
-name = data.find('nobr') # retrives name of team
-
-
-realm = table.find('a',{'class':'realm'}) # retrives server team plays on
+#data = table.find('a',{'class':'guild'}) # opens row with team data
+    
 current_raid = mythic_proggress.find('span',{'class':'selected'}) # retrives current raid name
-prog = table.find('span',{'class':'innerLink ratingProgress'}) # retrives current proggression 
 
-teams=[]
+names = table.find_all('nobr') # retrives name of team
+realms = table.find_all('a',{'class':'realm'}) # retrives server team plays on
+prog = table.find_all('span',{'class':'innerLink ratingProgress'}) # retrives current proggression 
+
+team_names = []
+team_realm = []
+current_prog = []
+
 team={}
-print(name)
+#print(name, realm, current_raid, prog)
+for ele in names:
+    for name in ele:
+        #print (name)
+        team_names.append(name)
+        
+for ele in realms:
+    for realm in ele:
+        #print (realm)
+        team_realm.append(realm)   
+        
+for ele in prog:
+    # full class
+    for proggress in ele:
+        # prog with tags
+        for i in proggress:   
+            # prog without tags
+            print (i)
+            current_prog.append(i)
+
+
+for ele in team_names,team_realm,current_prog:
+    print(team_names[0],team_realm[0],current_prog[0])
