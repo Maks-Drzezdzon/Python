5b1e0ec65c4e1f422084c9412e8b6ecb906b11d5
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-08-04 18:02:50 +0100

    lets get going with more APIs

--- Data Analysis/github_api_template.py
+++ Data Analysis/github_api_template.py
@@ -1,16 +1,26 @@
 # importing the requests library 
 import requests
 import json
+import pprint
 
-# gets request from api  
-r = requests.get('https://api.github.com/user', auth=('user', 'password'))
-# stores data from api in data
-data = r.json()
-# converst data to element
-json_text = json.dumps(data)
-# converst it to string format
+with open("C:/Users/Grim/Desktop/github_key.txt") as keys_file:
+    text = keys_file.readlines()
+
+user = text[0].strip().split(' ')[-1]
+password = text[1].strip().split(' ')[-1]
+
+# r = requests.get('https://api.github.com/user', auth=(user, password))
+
+
+r = requests.get('https://api.github.com/user/repos', auth=(user, password))
+
+json_text = json.dumps(r.json())
+
 request_response = json.loads(json_text)
-# store data in dict
-dictionary ={"my_id": request_response['id'],"my_login": request_response['login']}
 
-print(dictionary)
+# dictionary ={"my_id": request_response['id'],"my_login": request_response['login']}
+
+# pprint.pprint(request_response[0], indent = 4)
+
+for value in request_response[0]:
+    print(value)
