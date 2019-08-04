115daefd1f39ed4056acc860891140602a2c7984
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-08-04 19:19:06 +0100

    extracting data using dict comprehension

--- Data Analysis/github_api_template.py
+++ Data Analysis/github_api_template.py
@@ -15,12 +15,13 @@ password = text[1].strip().split(' ')[-1]
 r = requests.get('https://api.github.com/user/repos', auth=(user, password))
 
 json_text = json.dumps(r.json())
-
 request_response = json.loads(json_text)
 
 # dictionary ={"my_id": request_response['id'],"my_login": request_response['login']}
 
 # pprint.pprint(request_response[0], indent = 4)
 
-for value in request_response[0]:
-    print(value)
+
+value = request_response[0] # for reuse                
+github_api_links = {key : value[key] for key in value if "url" in key if value[key] != None}
+pprint.pprint(github_api_links)
