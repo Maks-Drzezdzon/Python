ee1d03aaff38840e8a66f18d7ca7136b8339b881
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-08-05 18:36:02 +0100

    looking at more api

--- Data Analysis/API/github_api_template.py
+++ Data Analysis/API/github_api_template.py
@@ -2,6 +2,7 @@
 import requests
 import json
 import pprint
+from requests.exceptions import HTTPError
 
 with open("C:/Users/Grim/Desktop/github_key.txt") as keys_file:
     text = keys_file.readlines()
@@ -22,12 +23,30 @@ def search_user(user,password):
     
     repository_names = [name['full_name'] for name in github_data]                
     github_api_links = {key : value[key] for key in value if "url" in key if value[key] != None}
+    link = [value[key] for key in value if "url" in key if value[key] != None]
     
+    return link
+    
+links = search_user(user,password)
+'''for link in links:
+    break
+    try:
+        response = requests.get(link)
+        response.raise_for_status()
+    # 
+    except HTTPError as http_err:
+        print(http_err)
+    except Exception as err:
+        print(err)
+    else:
+        print("connection made")'''
+        
+resp = requests.delete('https://api.github.com/user/repos/Hello-World', auth=(user, password))
+#pprint.pprint(json.dumps(resp.json()))
+pprint.pprint(resp)
 
-    
-search_user(user,password)
-data = {
-  "name": "Hello-World",
+'''data = {
+  "name": "test-repo",
   "description": "This is your first repository",
   "homepage": "https://github.com",
   "private": False,
@@ -36,9 +55,11 @@ data = {
   "has_wiki": True
 }
 
-payload={"name":"plswork"}
-#r = requests.get('https://api.github.com/user/repos?visibility=private', auth=(user, password))
-r = requests.post('https://api.github.com/user/repos',data=json.dumps(payload), auth=(user, password))
+payload={"name":"test"}
+r = requests.get('https://api.github.com/user/repos?visibility=private', auth=(user, password))
+r = requests.post('https://api.github.com/user/repos',data=json.dumps(data), auth=(user, password))
 data = json.dumps(r.json())
-pprint.pprint(data)
+pprint.pprint(data)'''
+
+
 
