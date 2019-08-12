2b1a7ebd0b77c6a1689b28ea6574e672b33f90e0
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-08-12 13:10:07 +0100

    quick recap of api

--- Data Analysis/API/httpbin.py
+++ Data Analysis/API/httpbin.py
@@ -3,29 +3,37 @@ import json
 from requests.exceptions import HTTPError
 import pprint
 
-res = requests.get('https://httpbin.org/get', params = json.dumps({'key1':'data1'}))
+
+url = 'https://httpbin.org'
+# session = requests.Session()
+# session.auth('user','pass')
+
+
+res = requests.get(url+'/get', params = json.dumps({'key1':'data1'}))
 #print(res.json())
 d = {'key2': ['data2','data3','data4']}
 
-res = requests.post('https://httpbin.org/post', params = json.dumps(d))
+res = requests.post(url+'/post', params = json.dumps(d))
 
 # print(res.text)
 patch_data = {'key2':['data1','data2',]}
 j_data = {'key3':'data',
           'key4': ['data2','data3','data4']}
 
-r = requests.post('https://httpbin.org/post', params = patch_data)
-f = requests.post('https://httpbin.org/post', json = j_data)
-a = requests.patch('https://httpbin.org/patch', params = patch_data , data = d)
+r = requests.post(url+'/post', params = patch_data)
+f = requests.post(url+'/post', json = j_data)
+a = requests.patch(url+'/patch', params = patch_data , data = d)
 # pprint.pprint(res.json())
 # pprint.pprint(r.headers)
 # pprint.pprint(a.json())
 # pprint.pprint(f.json())
 
 data = 200
-c = requests.get('https://httpbin.org/status/'+ str(data) )
-print(c.headers)
-
+c = requests.get(url+'/status/'+ str(data) )
+# print(c.headers)
+a = requests.get(url+'/ip')
+a.headers.update({'test':'testServer'})
+rep = requests.get(url+'/headers')
 
 
 
