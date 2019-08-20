eaf4f8a2c9d507742c5bc4aac0b394feba46a5f0
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-08-20 22:02:37 +0100

    fixed type error

--- Learning TensorFlow/student-mat.py
+++ Learning TensorFlow/student-mat.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import keras
 import pandas as pd
 import numpy as np
+import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
 
@@ -18,7 +19,14 @@ label_grade = "G3"
 x = np.array(data.drop([label_grade], 1))
 y = np.array(data[label_grade])
 
-x_train, y_train, x_test, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
+
+# splits data into 10% for testing
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split( x, y, test_size = 0.1)
+
+l = linear_model.LinearRegression()
+l.fit(x_train, y_train)
+accuracy = l.score(x_test, y_test)
+# print(accuracy) 88.6% accurate
 
 
 
