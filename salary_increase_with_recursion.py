71eb007eda0e00442291c9824f66da8a4e3ece4d
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-09-01 15:39:21 +0100

    some more python ai and data work

--- salary_increase_with_recursion.py
+++ salary_increase_with_recursion.py
@@ -2,7 +2,8 @@ def rec(time_years, salary, percent_per_year):
 	if time_years < 1:
 		return salary
 	else:
-		tmp = (((salary/100) * percent_per_year) + salary)
+		tmp = ((((salary/100) * percent_per_year) * 2) + salary)
 		return rec(time_years - 1, tmp, percent_per_year)
 
 	    
+print(rec(2,29000,3))
