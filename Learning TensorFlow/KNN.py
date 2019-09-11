0362e48b0bdd4eff9248ddbeb41d8d6d52280da9
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-09-11 22:43:57 +0100

    small updates to KNN code

--- Learning TensorFlow/KNN.py
+++ Learning TensorFlow/KNN.py
@@ -9,7 +9,7 @@ from sklearn import linear_model, preprocessing
 # note pandas reads in the first line of your data file as the col
 
 data = pd.read_csv('car.data')
-print(data.head())
+# print(data.head())
 
 # take lables and encode to int so 
 # operation can be performed 
@@ -18,7 +18,18 @@ pe = preprocessing.LabelEncoder()
 # mapping data to col name to convert
 buying = pe.fit_transform(list(data["buying"]))
 maint = pe.fit_transform(list(data["maint"]))
+door = pe.fit_transform(list(data["door"]))
+persons = pe.fit_transform(list(data["persons"]))
 lug_boot = pe.fit_transform(list(data["lug_boot"]))
 safety = pe.fit_transform(list(data["safety"]))
 clas = pe.fit_transform(list(data["class"]))
+# print(buying)
+predict = "class"
 
+X = list(zip(buying, maint, door, persons, lug_boot, safety))
+Y = list(clas)
+
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size = 0.1)
+
+# print(x_train, y_test)
