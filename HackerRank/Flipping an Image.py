f242a306289b44593b8e6112d8ad4f5fc03ac385
Maksthorn <Maksdrzezdzon@gmail.com>
2019-10-01 19:55:50 +0100

    leetcode runtime comparison is kind of a joke

--- HackerRank/Flipping an Image.py
+++ HackerRank/Flipping an Image.py
@@ -2,6 +2,7 @@
 class Solution:
     def flipAndInvertImage(self, A: List[List[int]]) -> List[List[int]]:
         return list([[1 - ele for ele in List][::-1] for List in A])
+        """or return [[ele ^ 1 for ele in List][::-1] for List in A] leet code doesnt seem to want to tell me which is quicker"""
 
 
         
