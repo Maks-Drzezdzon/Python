d87d0182b88b63e582a1711e2836ea8376e5aa3f
Maksthorn <Maksdrzezdzon@gmail.com>
2019-10-06 20:20:04 +0100

    some more python

---
+++ HackerRank/Self Dividing Numbers.py
@@ -0,0 +1,22 @@
+#  https://leetcode.com/problems/self-dividing-numbers/submissions/ 95.00
+
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        l=[]
+        
+        for ele in range(left, right+1):
+            string = str(ele)
+            str_len = len(string)
+            count=0
+            if not '0' in string:
+                for number in string:
+
+                    if ele % int(number) == 0:
+                        count += 1
+                    else:
+                        break
+                    if count == str_len:
+                        l.append(ele)
+                           
+        return l
+                             
