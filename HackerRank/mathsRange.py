b39d7f8389968c1ba3390da2a293519ab585a722
maksthorn <maksdrzezdzon@gmail.com>
2019-10-23 18:01:54 +0100

    interview

--- HackerRank/mathsRange.py
+++ HackerRank/mathsRange.py
@@ -1,16 +1,18 @@
 def solution(A, B):
     '''
-    this was meant to take a range of elements 6 - 20
-    and return if any, pairs of numbers that * to form the numbers E.G
-    with formula N * ( N + 1)
+    this was meant to take a range of elements i.e 6 - 20 inclusive
+    and return if any, pairs of numbers that conform with formula of
+    N * ( N + 1)
     
     in range 6,20
     6=2*3, 12=3*4, 20=4*5
     
+    and return how many pairs can be made
+    
     '''
     counter=0
     
-    for ele in range(A, B +1):
+    for ele in range(A, B + 1):
         result = math_helper_function(ele)
         if result == 1:
             counter +=1
@@ -27,5 +29,5 @@ def math_helper_function(numbers):
         else:
             pass
 
-
+# test cases
 print(solution(6,20))
