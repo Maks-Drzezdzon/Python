3f180bd28dc72c44739891d22fd16ed7cb1236d8
maksthorn <maksdrzezdzon@gmail.com>
2019-10-31 13:02:43 +0000

    interview prep

---
+++ HackerRank/tmp.py
@@ -0,0 +1,18 @@
+from collections import Counter
+def makeAnagram(s1, s2):
+    a = Counter(s1)
+    b = Counter(s2)
+    
+    c = a - b
+    #print(c)
+    d = b - a
+    print(d)
+    e = c + d
+    print(e)
+    return len(list(e.elements()))
+    
+
+
+s1="showman"
+s2="woman"
+makeAnagram(s1,s2)
