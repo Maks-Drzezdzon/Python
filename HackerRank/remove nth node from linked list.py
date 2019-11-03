6407b6f73b01976fee2198de801aa868653af67a
maksthorn <maksdrzezdzon@gmail.com>
2019-11-03 15:43:08 +0000

    commented

--- HackerRank/remove nth node from linked list.py
+++ HackerRank/remove nth node from linked list.py
@@ -2,20 +2,29 @@
 
 class Solution(object):
     def removeNthFromEnd(self, head, n):
-        p = head
-        i = 0
-        while p:
-            p = p.next
-            i += 1
-        p = head
-        step = i-n #total length - n
+        # variables
+        place = head
+        length = 0
+        
+        #iter over nodelist
+        while place:
+            place = place.next
+            length += 1
+        # count len of vars
+        # if len is 5, 4-2 = 2 delete 3rd element in node list
+        place = head
+        step = length - n 
+        # if its 0 delte next
         if step == 0:
             head = head.next
         else:
+            # otherwise iter over list till you get to step of 
             l = 1
-            while l<step :
-                p = p.next
+            while l < step:
+                place = place.next
                 l += 1
-            p.next = p.next.next
+            place.next = place.next.next
+            
         return head
+        
         
