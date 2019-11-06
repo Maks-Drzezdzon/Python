b59de5990526ef0e1ce9fb0ded9fa576cfb3a520
maksthorn <maksdrzezdzon@gmail.com>
2019-11-06 17:47:37 +0000

    quick prep

---
+++ HackerRank/Balanced Brackets.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the isBalanced function below.
+def isBalanced(s):
+    stack =[]
+    for ele in s:
+        if ele == '(' or ele == '[' or ele == '{':
+            stack.append(ele)
+        elif len(stack) == 0:
+                return "NO"
+        else:
+            bracket = stack.pop()
+            if bracket == '(' and ele != ')':
+                return "NO"
+            if bracket == '[' and ele != ']':
+                return "NO"
+            if bracket == '{' and ele != '}':
+                return "NO"
+
+    if len(stack) == 0:
+        return "YES"
+    else:
+        return "NO"
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        s = input()
+
+        result = isBalanced(s)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
