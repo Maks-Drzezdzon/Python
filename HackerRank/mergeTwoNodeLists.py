c6b1d3623095b2bce1b13c0203eb669ffdd4d0a8
maksthorn <maksdrzezdzon@gmail.com>
2019-11-29 18:16:29 +0000

    better version + java version

--- HackerRank/mergeTwoNodeLists.py
+++ HackerRank/mergeTwoNodeLists.py
@@ -5,25 +5,81 @@ class ListNode(object):
          self.val = x
          self.next = None
 
-class Solution(object):
-    def mergeTwoLists(self, head1, head2):
-        head = ptr = ListNode(0)
+
+def mergeTwoLists(self, head1: ListNode, head2: ListNode):
+    head = ptr = ListNode(0)
+    
+    if head1 == None:
+        return head2
+    if head2 == None:
+        return head1
+    
+    
+    # go through both lists and compare nodes
+    while(head1 and head2):
+        if (head1.val < head2.val):
+            ptr.next = head1
+            # set back to none
+            head1 = head1.next
+            # moves pointer to next val
+            
+        elif (head1.val >= head2.val):
+            ptr.next = head2
+            head2 = head2.next
+            # moves pointer to next val
+        ptr = ptr.next
+    
+    # if one list is longer than the other finish the rest of the list with the longer list since they are already sorted 
+    ptr.next = head1 or head2
+    return head.next
+
+'''
+# java version 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode head1, ListNode head2) {
+        ListNode ptr = new ListNode(0);
+        ListNode head = ptr;
         
-        # go through both lists and compare nodes
-        while(head1 and head2):
-            if (head1.val < head2.val):
-                ptr.next = head1
-                head1 = head1.next
-                # stores prev value
-                ptr = ptr.next
-                
-            elif (head1.val >= head2.val):
-                ptr.next = head2
-                head2 = head2.next
-                # stores prev value
-                ptr = ptr.next
+        if(head1==null) {
+            return head2;
+        }
         
-        # if one list is longer than the other finish the rest of the list with the longer list since they are already sorted 
-        ptr.next = head1 or head2
-        return head.next
+        if(head2==null) {
+            return head1;
+        }
         
+        while(head1 != null && head2 != null){
+            if(head1.val <= head2.val) {
+                ptr.next = head1;
+                head1 = head1.next;
+            } else {
+                ptr.next = head2;
+                head2 = head2.next;
+            }
+            ptr = ptr.next;
+        }
+        
+        if(head1!=null) {
+            ptr.next = head1;
+        }
+        
+        if(head2!=null) {
+            ptr.next = head2;
+        }
+        
+        
+        return head.next;
+        
+    }
+}
+
+
+'''
