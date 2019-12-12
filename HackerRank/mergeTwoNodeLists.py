2c9bac259ea2db7d722b26b2567e383a31e8cfcb
maksthorn <maksdrzezdzon@gmail.com>
2019-12-12 19:20:55 +0000

    interview prep

--- HackerRank/mergeTwoNodeLists.py
+++ HackerRank/mergeTwoNodeLists.py
@@ -1,82 +1,33 @@
 # Definition for singly-linked list.
-# https://leetcode.com/problems/merge-two-sorted-lists/submissions/
-class ListNode(object):
-    def __init__(self, x):
-         self.val = x
-         self.next = None
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
-
-def mergeTwoLists(self, head1: ListNode, head2: ListNode):
-    head = ptr = ListNode(0)
-    
-    if head1 == None:
-        return head2
-    if head2 == None:
-        return head1
-    
-    # go through both lists and compare nodes
-    while(head1 and head2):
-        if (head1.val < head2.val):
+class Solution:
+    def mergeTwoLists(self, head1: ListNode, head2: ListNode) -> ListNode:
+        ptr = ListNode(0)
+        head = ptr
+        
+        if head1 == None:
+            return head2
+        if head2 == None:
+            return head1
+        
+        while head1 != None and head2 != None:
+            if head1.val <= head2.val:
+                ptr.next = head1
+                head1 = head1.next
+            else:
+                ptr.next = head2
+                head2 = head2.next
+            
+            ptr = ptr.next
+        
+        if head1 != None:
             ptr.next = head1
-            head1 = head1.next
-            
-        elif (head1.val >= head2.val):
+        if head2 != None:
             ptr.next = head2
-            head2 = head2.next
-            
-        ptr = ptr.next
+        
+        return head.next
     
-    # if one list is longer than the other finish the rest of the list with the longer list since they are already sorted 
-    ptr.next = head1 or head2
-    return head.next
-
-'''
-# java version 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
-class Solution {
-    public ListNode mergeTwoLists(ListNode head1, ListNode head2) {
-        ListNode ptr = new ListNode(0);
-        ListNode head = ptr;
-        
-        if(head1==null) {
-            return head2;
-        }
-        
-        if(head2==null) {
-            return head1;
-        }
-        
-        while(head1 != null && head2 != null){
-            if(head1.val <= head2.val) {
-                ptr.next = head1;
-                head1 = head1.next;
-            } else {
-                ptr.next = head2;
-                head2 = head2.next;
-            }
-            ptr = ptr.next;
-        }
-        
-        if(head1!=null) {
-            ptr.next = head1;
-        }
-        
-        if(head2!=null) {
-            ptr.next = head2;
-        }
-        
-        
-        return head.next;
-        
-    }
-}
-
-
-'''
