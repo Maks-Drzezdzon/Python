9167972f8959de08336591dca27e1ad2bca71264
maksthorn <maksdrzezdzon@gmail.com>
2019-12-22 18:43:54 +0000

    interview prep

--- HackerRank/Sock Merchant.py
+++ HackerRank/Sock Merchant.py
@@ -20,7 +20,16 @@ def sockMerchant(n, bag_of_socks):
           unique.add(sock)
        
     return len(pair)
-    
+
+def sockMerchantV2(n, ar):
+    pair, unique = 0, set(ar)
+    for ele in ar:
+        if ele in unique:
+            unique.remove(ele)
+        else: 
+            pair += 1
+            unique.add(ele)
+    return pair
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
