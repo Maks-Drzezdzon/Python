e4756f34f669eb7b353e6dcb47cad97321abd297
maksthorn <maksdrzezdzon@gmail.com>
2019-12-22 19:36:26 +0000

    interview prep

--- HackerRank/hourglass.py
+++ HackerRank/hourglass.py
@@ -8,13 +8,17 @@ import sys
 
 # Complete the hourglassSum function below.
 def hourglassSum(arr):
-    rows = len(arr) -2
-    cols = len(arr[0]) -2
+    # rows = len(arr) -2
+    # cols = len(arr[0]) -2
     maxval = - sys.maxsize -1
-
-    for row in range(rows):
-        for col in range(cols):
-            hourglass = arr[row][col] + arr[row][col+1] +arr[row][col+2]+arr[row+1][col+1]+arr[row+2][col] + arr[row+2][col+1] +arr[row+2][col+2]
+    for row in range(len(arr) -2):
+        for col in range(len(arr[0]) -2):
+            
+            hourglass = \
+            arr[row][col] + arr[row][col+1] + arr[row][col+2] \
+            + arr[row+1][col+1] + \
+            arr[row+2][col] + arr[row+2][col+1] + arr[row+2][col+2] 
+            
             maxval = max(maxval, hourglass)
     return maxval
 
