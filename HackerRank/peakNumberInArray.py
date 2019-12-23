0d2b101ca711272fcfc0ff535f0329d31ba29dae
maksthorn <maksdrzezdzon@gmail.com>
2019-12-23 18:31:16 +0000

    interview prep

--- HackerRank/peakNumberInArray.py
+++ HackerRank/peakNumberInArray.py
@@ -1,3 +1,10 @@
 class Solution:
-    def peakIndexInMountainArray(self, A: List[int]) -> int:
+    def peakIndexInMountainArray(self, A: list[int]) -> int:
         return A[int(len(A)/2)]
+    
+
+    def peakIndexInMountainArrayV2(self, A: list[int]) -> int:
+        i=0
+        while A[i] < A[i + 1]:
+            i += 1
+        return i
