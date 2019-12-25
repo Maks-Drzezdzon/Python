878adb6b09e0206509cfcad1980a40de5ca8cae3
maksthorn <maksdrzezdzon@gmail.com>
2019-12-25 17:08:12 +0000

    just adding some error catching

--- Data Analysis/Web Scraping/folder_for_storage/img_scrape.py
+++ Data Analysis/Web Scraping/folder_for_storage/img_scrape.py
@@ -1,12 +1,11 @@
 from bs4 import BeautifulSoup
 from requests import get
-import urllib.request
 from PIL import Image
 from io import BytesIO
 import os
 
-url = 'link here'
-response = get(url)
+
+response = get("url here")
 html_soup = BeautifulSoup(response.text, 'html.parser')
 # the tag
 # and class
@@ -17,11 +16,14 @@ for ele in data:
     extracted_link = str(ele).split('"')[-2]
     extracted_file_name = str(ele).split('/')[-2].replace('"','')
     r = get(extracted_link)
-    
-    i = Image.open(BytesIO(r.content))
-    print(extracted_file_name)
-    # ./ save in current folder
-    # ../ one level above
-    i.save(os.path.join('./', extracted_file_name))
-    
-    
+    if r.status_code == 200:
+        try:
+            i = Image.open(BytesIO(r.content))
+            print("[*] Downloaded " + extracted_file_name)
+            # ./ save in current folder
+            # ../ one level above
+            i.save(os.path.join('./', extracted_file_name))
+        except Exception as e:
+            print(e)
+    else:
+        print("Error with get request")
