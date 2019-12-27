67dc89c7270b5778fc5afc2c4bb9f4579d554227
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-12-27 15:49:02 +0000

    interview prep

---
+++ HackerRank/reverseLinkedList.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        previous = None
+        current = head
+        Next = None
+        
+        while current != None:
+            Next = current.next
+            current.next = previous
+            previous = current
+            current = Next 
+        
+        
+        return previous
