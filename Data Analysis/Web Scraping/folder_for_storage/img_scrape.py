938cb73131b86ef340fd2e2195e2caa17e6dee2b
maksthorn <maksdrzezdzon@gmail.com>
2019-12-29 17:47:47 +0000

    adding some proc for video scraping

--- Data Analysis/Web Scraping/folder_for_storage/img_scrape.py
+++ Data Analysis/Web Scraping/folder_for_storage/img_scrape.py
@@ -4,26 +4,52 @@ from PIL import Image
 from io import BytesIO
 import os
 
+def img(url):
+    response = get(url)
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+    # the tag
+    # and class
+    data = html_soup.find_all('img', class_ = 'img-back lasyload')
+    
+    # find the data that you want and pass it to write
+    for ele in data:
+        extracted_link = str(ele).split('"')[-2]
+        extracted_file_name = str(ele).split('/')[-2].replace('"','')
+        r = get(extracted_link)
+        if r.status_code == 200:
+            try:
+                i = Image.open(BytesIO(r.content))
+                print("[*] Downloaded " + extracted_file_name)
+                # ./ save in current folder
+                # ../ one level above
+                i.save(os.path.join('./', extracted_file_name))
+            except Exception as e:
+                print(e)
+        else:
+            print("Error with get request")
 
-response = get("url here")
-html_soup = BeautifulSoup(response.text, 'html.parser')
-# the tag
-# and class
-data = html_soup.find_all('img', class_ = 'img-back lasyload')
-
-# find the data that you want and pass it to write
-for ele in data:
-    extracted_link = str(ele).split('"')[-2]
-    extracted_file_name = str(ele).split('/')[-2].replace('"','')
-    r = get(extracted_link)
-    if r.status_code == 200:
-        try:
-            i = Image.open(BytesIO(r.content))
-            print("[*] Downloaded " + extracted_file_name)
-            # ./ save in current folder
-            # ../ one level above
-            i.save(os.path.join('./', extracted_file_name))
-        except Exception as e:
-            print(e)
-    else:
-        print("Error with get request")
+def vid(url):
+    response = get(url)
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+    # the tag
+    # and class
+    data = html_soup.find_all('div', class_ = 'video')
+    print(data[:500])
+    
+    # find the data that you want and pass it to write
+    for ele in data:
+        extracted_link = str(ele).split('"')[-2]
+        extracted_file_name = str(ele).split('/')[-2].replace('"','')
+        r = get(extracted_link)
+        if r.status_code == 200:
+            try:
+                i = Image.open(BytesIO(r.content))
+                print("[*] Downloaded " + extracted_file_name)
+                # ./ save in current folder
+                # ../ one level above
+                i.save(os.path.join('./', extracted_file_name))
+            except Exception as e:
+                print(e)
+        else:
+            print("Error with get request")
+            
