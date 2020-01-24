30f9ab6f0d2f64ce33f4cc6acb278b2e8996df75
maksthorn <maksdrzezdzon@gmail.com>
2020-01-24 18:24:50 +0000

    layout

--- Data Analysis/Data Analysis/adaptive_mobile_project.py
+++ Data Analysis/Data Analysis/adaptive_mobile_project.py
@@ -8,7 +8,6 @@ def read_file(path):
     data = pd.read_csv(path) 
     print(data)
 
-
 # read_file(path)
 
 def to_tsv(path):
@@ -17,17 +16,11 @@ def to_tsv(path):
     '''
     file = path.split('/')[-1]
     with open(file, 'r') as myfile:
-      with open("test.csv", 'w') as csv_file:
+      with open("test.txt", 'w') as csv_file:
         for line in myfile:
+          # fileContent = re.sub("-", "\t", line)
           fileContent = re.sub(",", "\t", line)
           csv_file.write(fileContent)
-
-def test_to_tsv():
-    print(detect('test.csv','\t'))
-
-# to_tsv(path)
-test_to_tsv()
-
 
 def data_sample():
     '''
@@ -41,6 +34,31 @@ def min_max_date_time():
     '''
     pass
 
+def no_duplicated_ids():
+    '''
+    5) Let us suppose we don't want duplicated "ID"s. In case there are rows 
+   with duplicated IDs we just keep the first row we've found: how would you 
+   extract a file, from the original one, without duplicated IDs?
+    '''
+    pass
+
+
+def to_unix_timestamp():
+    '''
+    6) How would you create a new file where the "Datetime" column is substituted
+    with a "Timestamp" column with the (Unix) timestamp in milliseconds?
+    i.e., for the example above you would have:
+    '''
+    pass
+   
+    
+def count_orderby():
+    '''
+    7) How would you create a simple count of the distinct "SourceType"s? 
+    i.e. we want to see each distinct "SourceType", with the number
+    of times it appears in the file, sorted by descending number of occurencies.
+    '''
+    pass
 
 
 '''
@@ -48,7 +66,7 @@ Questions:
 1) Which considerations can you make about the input data? 
     # i have no idea ? check it conforms to a pattern ?
     If the file size is big (let’s say 20GB), how would store it? 
-    # it depends on what kind of data it is, database with relations
+    # it depends on what kind of data it is
     # compress it
     
     Would you keep the original format? If not, what would you do? 
@@ -56,24 +74,4 @@ Questions:
     Could you describe different options/format for storing the data? 
     
     Which sanity checks would you apply and why?
-    
-    
-    
-5) Let us suppose we don't want duplicated "ID"s. In case there are rows 
-   with duplicated IDs we just keep the first row we've found: how would you 
-   extract a file, from the original one, without duplicated IDs?
-   
-6) How would you create a new file where the "Datetime" column is substituted
-    with a "Timestamp" column with the (Unix) timestamp in milliseconds?
-    i.e., for the example above you would have:
-        
-ID,Timestamp,SourceType,DestinationType,MessageType
-723649,1520793122.321,ES-010,DS-020,PK_723_AB
-184734,1509613424.524,ES-201,DS-173,PK_163_RF
-631838,1516401757.183,ES-150,DS-155,PK_126_RP
-
-7) How would you create a simple count of the distinct "SourceType"s? 
-    i.e. we want to see each distinct "SourceType", with the number
-    of times it appears in the file, sorted by descending number of occurencies.
-
 '''
