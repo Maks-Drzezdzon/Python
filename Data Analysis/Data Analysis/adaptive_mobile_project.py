8f1419c95128a06e2937eb471b513722582c11fb
maksthorn <maksdrzezdzon@gmail.com>
2020-01-24 17:50:56 +0000

    moving around and testing

--- Data Analysis/Data Analysis/adaptive_mobile_project.py
+++ Data Analysis/Data Analysis/adaptive_mobile_project.py
@@ -1,37 +1,79 @@
 import pandas as pd
+import re
+from detect_delimiter import detect
+
+path = "../Data/adaptive_mobile_dataset.csv"
+
+def read_file(path):
+    data = pd.read_csv(path) 
+    print(data)
 
 
-data = pd.read_csv("../Data/adaptive_mobile_dataset.csv") 
-print(data)
+# read_file(path)
+
+def to_tsv(path):
+    '''
+    2) How would transform this file into a TSV file? (i.e. a file where the comma separator ',' is substituted with a tab character '\t')
+    '''
+    file = path.split('/')[-1]
+    with open(file, 'r') as myfile:
+      with open("test.csv", 'w') as csv_file:
+        for line in myfile:
+          fileContent = re.sub(",", "\t", line)
+          csv_file.write(fileContent)
+
+def test_to_tsv():
+    print(detect('test.csv','\t'))
+
+# to_tsv(path)
+test_to_tsv()
 
 
+def data_sample():
+    '''
+    3) How would you extract a random sample of 5% of the rows from this file?
+    '''
+    pass
 
-
+def min_max_date_time():
+    '''
+    4) How would extract the minimum and maximum value from the "Datetime" column?
+    '''
+    pass
 
 
 
 '''
-Let's suppose you have are given a CSV file, with header, that needs to be prepared for further analysis.
-The first rows could be like:
-
-ID,Datetime,SourceType,DestinationType,MessageType
-723649,2018-03-11 18:32:02.321,ES-010,DS-020,PK_723_AB
-184734,2017-11-02 09:03:44.524,ES-201,DS-173,PK_163_RF
-631838,2018-01-19 22:42:37.183,ES-150,DS-155,PK_126_RP
-
 Questions:
-1) Which considerations can you make about the input data? If the file size is big (let’s say 20GB), how would store it? Would you keep the original format? If not, what would you do? Could you describe different options/format for storing the data? Which sanity checks would you apply and why?
-2) How would transform this file into a TSV file? (i.e. a file where the comma separator ',' is substituted with a tab character '\t')
-3) How would you extract a random sample of 5% of the rows from this file?
-4) How would extract the minimum and maximum value from the "Datetime" column?
-5) Let us suppose we don't want duplicated "ID"s. In case there are rows with duplicated IDs we just keep the first row we've found: how would you extract a file, from the original one, without duplicated IDs?
-6) How would you create a new file where the "Datetime" column is substituted with a "Timestamp" column with the (Unix) timestamp in milliseconds? i.e., for the example above you would have:
+1) Which considerations can you make about the input data? 
+    # i have no idea ? check it conforms to a pattern ?
+    If the file size is big (let’s say 20GB), how would store it? 
+    # it depends on what kind of data it is, database with relations
+    # compress it
+    
+    Would you keep the original format? If not, what would you do? 
+    
+    Could you describe different options/format for storing the data? 
+    
+    Which sanity checks would you apply and why?
+    
+    
+    
+5) Let us suppose we don't want duplicated "ID"s. In case there are rows 
+   with duplicated IDs we just keep the first row we've found: how would you 
+   extract a file, from the original one, without duplicated IDs?
+   
+6) How would you create a new file where the "Datetime" column is substituted
+    with a "Timestamp" column with the (Unix) timestamp in milliseconds?
+    i.e., for the example above you would have:
+        
 ID,Timestamp,SourceType,DestinationType,MessageType
 723649,1520793122.321,ES-010,DS-020,PK_723_AB
 184734,1509613424.524,ES-201,DS-173,PK_163_RF
 631838,1516401757.183,ES-150,DS-155,PK_126_RP
-7) How would you create a simple count of the distinct "SourceType"s? i.e. we want to see each distinct "SourceType", with the number of times it appears in the file, sorted by descending number of occurencies.
 
-Note: you can assume you always have enough space on disk to save any intermediate and final data.
+7) How would you create a simple count of the distinct "SourceType"s? 
+    i.e. we want to see each distinct "SourceType", with the number
+    of times it appears in the file, sorted by descending number of occurencies.
 
 '''
