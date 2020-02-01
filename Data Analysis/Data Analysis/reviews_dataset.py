64374d00badbec6489b48250a61d02ca7cd861eb
maksthorn <maksdrzezdzon@gmail.com>
2020-02-01 01:01:27 +0000

    cleaning dataset

--- Data Analysis/Data Analysis/reviews_dataset.py
+++ Data Analysis/Data Analysis/reviews_dataset.py
@@ -4,5 +4,7 @@ import re
 
 reviews_dataset_path = "../Data/reviews.csv"
 reviews_dataset = pd.read_csv(reviews_dataset_path, error_bad_lines=False, lineterminator='\n')
-print(reviews_dataset)
-    
+reviews_dataset.columns = ['A', 'B', 'C', 'D' , 'E', 'F']
+
+t = reviews_dataset.drop_duplicates(subset='A', keep="first", inplace=True)
+print(t)
