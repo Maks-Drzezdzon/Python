275a693a6f1b81c8972bdc924e648c7135f564bb
maksthorn <maksdrzezdzon@gmail.com>
2020-02-08 01:21:02 +0000

    preparing data

---
+++ Data Analysis/Data Analysis/crowds_cure_cancer_2017.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import re
+
+
+crowds_cure_cancer_dataset_path = "../Data/crowds_cure_cancer_2017.csv"
+crowds_cure_cancer_dataset = pd.read_csv(crowds_cure_cancer_dataset_path)
+
+
+print(crowds_cure_cancer_dataset)
+
