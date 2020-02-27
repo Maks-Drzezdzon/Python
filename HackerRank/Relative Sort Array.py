9c2369a6303d6fe9e7dad6a7c1a45d8cc91f68af
maksthorn <maksdrzezdzon@gmail.com>
2020-02-27 21:40:24 +0000

    .

--- HackerRank/Relative Sort Array.py
+++ HackerRank/Relative Sort Array.py
@@ -3,21 +3,22 @@ import collections
 def relativeSortArray(arr1, arr2):
     c = collections.Counter(arr1)
     # c only has the unused elements since they were not popped
-    res = [[i]*c.pop(i) for i in arr2] 
+    result = [[i]*c.pop(i) for i in arr2] 
     # res returns a list of lists because of [i], this is because
     # in a list comp each val would for 1 would have been added to itself rather than appended to a list
     # and would look like this
     # [3,4,3,12]
     # return a flattened list
-    return [lis for sublist in res for lis in sublist] + sorted(c.elements())
+    return [lis for sublist in result for lis in sublist] + sorted(c.elements())
 
 
 # print(relativeSortArray([1,1,4,4,2,1,2,3,4],[1,2,3,4]))
 
 
-test = [1,2,2,3,3,3,3]
+test = [1,2,2,3,3,3,3,5,5]
 test_list = [3,2,1]
-a = collections.Counter(t)
+a = collections.Counter(test)
 # print([i*a.pop(i) for i in tt])
+print([ele for ele in a])
 print(2*a.pop(3))
 print(a)
