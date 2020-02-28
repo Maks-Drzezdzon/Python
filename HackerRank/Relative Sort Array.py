90ae3b4eb6a776b0be12fd93d5ab6aa43ae9fb47
maksthorn <maksdrzezdzon@gmail.com>
2020-02-28 21:18:46 +0000

    new method woop

--- HackerRank/Relative Sort Array.py
+++ HackerRank/Relative Sort Array.py
@@ -1,4 +1,6 @@
 import collections
+from functools import reduce
+import operator
 
 def relativeSortArray(arr1, arr2):
     c = collections.Counter(arr1)
@@ -9,16 +11,20 @@ def relativeSortArray(arr1, arr2):
     # and would look like this
     # [3,4,3,12]
     # return a flattened list
-    return [lis for sublist in result for lis in sublist] + sorted(c.elements())
+    # return [lis for sublist in result for lis in sublist] + sorted(c.elements())\
+    # performs the same action as above just with less code
+    # reduce performs adding of all lists together to reduce to 1 list
+    return reduce(operator.add, result) + sorted(c.elements())
 
 
-# print(relativeSortArray([1,1,4,4,2,1,2,3,4],[1,2,3,4]))
+print(relativeSortArray([1,1,4,4,2,1,2,3,4],[1,2,3,4]))
 
-
+'''
 test = [1,2,2,3,3,3,3,5,5]
 test_list = [3,2,1]
 a = collections.Counter(test)
-# print([i*a.pop(i) for i in tt])
+print([i*a.pop(i) for i in tt])
 print([ele for ele in a])
 print(2*a.pop(3))
 print(a)
+'''
