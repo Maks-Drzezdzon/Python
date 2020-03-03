aa306dde9091c2e744b9f73266167a7f349824d9
maksthorn <maksdrzezdzon@gmail.com>
2020-03-03 18:25:51 +0000

    .

--- HackerRank/Find Numbers with Even Number of Digits.py
+++ HackerRank/Find Numbers with Even Number of Digits.py
@@ -1,5 +1,14 @@
 class Solution:
     def findNumbers(self, nums: List[int]) -> int:
+        '''
         tmp = [str(ele).replace('', ' ').split() for ele in nums if not None]
         answer = [ele for ele in [str(ele).replace('', ' ').split() for ele in nums if not None] if len(ele) % 2 == 0]
         return len(answer)
+        '''
+        
+        answer = 0
+        for num in nums:
+            if not len(str(num)) % 2:
+                answer += 1
+        return answer
+        
