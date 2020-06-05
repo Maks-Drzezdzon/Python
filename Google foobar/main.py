a8a2097aa7c65267599e8634a9d7de8c8e88d779
maksthorn <maksdrzezdzon@gmail.com>
2020-06-05 16:53:36 +0100

    wooop i did it, sort of

--- Google foobar/main.py
+++ Google foobar/main.py
@@ -1,4 +1,8 @@
 
+
+from collections import Counter
+
+
 def foobar1(x):
     d = {
         'z': 'a', 'y': 'b', 'x': 'c',
@@ -22,3 +26,35 @@ def foobar1(x):
             a = a + letter
 
     return a.strip(" ")
+
+
+def foobar2(s):
+    # Your code here
+    a = "".join([letter for letter in s if letter is not '-']
+                ).lstrip("<").rstrip(">")
+    a = list(a)
+
+    counter = 0
+
+    left_stack = a.copy()
+    right_stack = []
+
+    while left_stack:
+        f = left_stack.pop(0)
+        right_stack.append(f)
+
+        if f == '>':
+            for letter in left_stack:
+                if f != letter:
+                    counter += 1
+        elif f == '<':
+            for letter in right_stack:
+                if f != letter:
+                    counter += 1
+    return counter
+
+
+a = "--->-><-><-->-"
+print(foobar2(">><><>"))  # 10
+print(foobar2("<<>><"))  # 4
+print(foobar2(">----<"))  # 2
